8bfa006fff7ae9c6c22167d509f94885ff16bc7e
Samantha Wolff <samanthawolff07@gmail.com>
2022-04-30 23:02:52 -0500

    getting userChoices prompts to work

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
 // Import the sequilize constructor from the library
 const mysql = require('mysql2');
 
-require('dotenv').config();
+// require('./env').config();
 
 // Create connection to database
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: process.env.DB_PW,
+    password: '1245Sammi',
     database: 'employees'
 });
     
--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 const connection = require('./config/connection');
 const inquirer = require('inquirer');
-const consoleTable = require('connection.table');
+const consoleTable = require('console.table');
+
 
 connection.connect((error) => {
     if (error) throw error;
     console.log('Database Connected!');
 });
+
+const promptUser = () => {
+    return inquirer.prompt([
+
+        {
+            type: 'list',
+            name: 'userChoices',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role'
+            ]
+        }
+    ])
+};
+
+promptUser();
